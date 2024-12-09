13696cef20e68ceaca6d4cf2fda7e793ab1a0c17
libertykmc <axmedm@inbox.ru>
2024-12-09 20:46:08 +0300

    Minor

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,70 +1,69 @@
 <template>
-    <div class="app">
-      <img src="/chill.jpg" alt="" class="photo" />
-      <h1>Эй мужик, руби бабло!</h1>
-      <p>Бабос: {{ count }}</p>
-      <button @click="increment">Зарабатывать!</button>
-      </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent, ref, onMounted, watch } from "vue";
-  import { tg, initTelegram } from "../telegram";
-  
-  export default defineComponent({
-    name: "App",
-    setup() {
-      const count = ref<number>(parseInt(localStorage.getItem("count") || "0"));
-      const isTelegram = ref(!!tg);
-  
-      const increment = () => {
-        count.value++;
-      };
-  
-      const closeApp = () => {
-        if (tg) tg.close();
-      };
-  
-      onMounted(() => {
-        initTelegram();
-        if (tg) {
-          tg.MainButton.text = "Start Clicking!";
-          tg.MainButton.show();
-        }
-      });
-  
-      watch(count, (newCount) => {
-        localStorage.setItem("count", newCount.toString());
-      });
-  
-      return { count, increment, closeApp, isTelegram };
-    },
-  });
-  </script>
-  
-  <style>
-  .app {
-    text-align: center;
-    margin-top: 20vh;
-  }
-  button {
-    padding: 10px 20px;
-    font-size: 16px;
-    margin: 10px;
-  }
-  .photo {
-    width: 200px;
-  }
-  
-  .app__nav {
-    display: flex;
-    gap: 30px;
-    justify-content: center;
-  }
-  .app__link {
-    text-decoration: none;
-    color: inherit;
-    font-size: 20px;
-  }
-  </style>
-  
+  <div class="app">
+    <img src="/icon.png" alt="" class="photo" />
+    <h1>Эй мужик, руби бабло!</h1>
+    <p>Бабос: {{ count }}</p>
+    <button @click="increment">Зарабатывать!</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted, watch } from "vue";
+import { tg, initTelegram } from "../telegram";
+
+export default defineComponent({
+  name: "App",
+  setup() {
+    const count = ref<number>(parseInt(localStorage.getItem("count") || "0"));
+    const isTelegram = ref(!!tg);
+
+    const increment = () => {
+      count.value++;
+    };
+
+    const closeApp = () => {
+      if (tg) tg.close();
+    };
+
+    onMounted(() => {
+      initTelegram();
+      if (tg) {
+        tg.MainButton.text = "Start Clicking!";
+        tg.MainButton.show();
+      }
+    });
+
+    watch(count, (newCount) => {
+      localStorage.setItem("count", newCount.toString());
+    });
+
+    return { count, increment, closeApp, isTelegram };
+  },
+});
+</script>
+
+<style>
+.app {
+  text-align: center;
+  margin-top: 20vh;
+}
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  margin: 10px;
+}
+.photo {
+  width: 200px;
+}
+
+.app__nav {
+  display: flex;
+  gap: 30px;
+  justify-content: center;
+}
+.app__link {
+  text-decoration: none;
+  color: inherit;
+  font-size: 20px;
+}
+</style>
